6cb0e80e979d3c871c7c6d33e9cca748126aaf22
zyc404 <zyc404@gmail.com>
2024-05-26 19:26:35 +0800

    更换version获取方法

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import path from 'path'
+import fs from 'fs'
 import { logger, common } from '#Karin'
-import packageConfig from './package.json' assert { type: 'json' };
 
 /** 当前文件的绝对路径 */
 const filePath = common.absPath(import.meta.url.replace(/^file:(\/\/\/|\/\/)/, ''))
 /** 插件包的目录路径 */
 const dirname = path.dirname(filePath)
 /** 插件包的名称 */
-const basename = packageConfig.name
-/** 插件包的版本 */
-const version = packageConfig.version
+const basename = path.basename(dirname)
 /** 插件包相对路径 */
 const dirPath = './plugins/' + basename
+
+const _package = JSON.parse(fs.readFileSync(dirPath + '/package.json', 'utf8'))
+/** 插件包的版本 */
+const version = _package.version
 
 export { dirPath }
 
